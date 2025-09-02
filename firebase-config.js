d04460f220ab005c8b358dc23c8db98577901242
willy9015 <navaeste.wt@gmail.com>
2025-09-02 13:23:02 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Completa con tus claves si deseas nube y subidas.
+// Si lo dejas vacío, todo funciona localmente (enlace/QR siguen funcionando).
+
+window.FIREBASE_CONFIG = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
